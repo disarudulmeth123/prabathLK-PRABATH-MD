cc8a0d70ef111dea8ad713f474eab8d294003dc3
disarudulmeth123 <disarudulmeth123@gmail.com>
2025-05-06 00:46:00 +0530

    Update config-env.js

--- config-env.js
+++ config-env.js
@@ -20,9 +20,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Enter your session-id",
-BOT_NUMBER: process.env.BOT_NUMBER || "Enter your botnumber (without + mark and spaces)",
-PASSWORD: process.env.PASSWORD || "Enter you like password",
-GITHUB_USERNAME: process.env.GITHUB_USERNAME || "Enter your github username",
-GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || "Enter your GITHUB_AUTH_TOKEN"
+SESSION_ID: process.env.SESSION_ID || "PRABATH-MD~pzExhIDS#c28Y8GsuGox0uw-oP-BR48klcDwA90lqJHehqnnFow8",
+BOT_NUMBER: process.env.BOT_NUMBER || "Enter your botnumber (94753390944)",
+PASSWORD: process.env.PASSWORD || "123**##",
+GITHUB_USERNAME: process.env.GITHUB_USERNAME || "disarudulmeth123",
+GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || "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"
 };
